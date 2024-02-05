9d007ad66930646b95880716844c7d9a5e4233fb
Hugo Maurinier <hugo.maurinier@epitech.eu>
2024-02-05 12:06:49 +0100

    [ADD] Models

---
+++ olympic_warriors/management/commands/createsu.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+from olympic_warriors.settings import settings
+
+
+class Command(BaseCommand):
+    help = "Creates a superuser."
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username=settings.SU_USERNAME).exists():
+            User.objects.create_superuser(
+                username=settings.SU_USERNAME, password=settings.SU_PASSWORD
+            )
+        print("Superuser has been created.")
--- olympic_warriors/models.py
+++ olympic_warriors/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Player(models.Model):
+    """
+    A player is a user that has a rating and is part of a team.
+    """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(10)])
+    team = models.ForeignKey("Team", on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+
+class Team(models.Model):
+    """
+    A team is a group of players that participate in an edition of the Olympic Warriors.
+    """
+
+    name = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=True)
+
+
+class Edition(models.Model):
+    """
+    An edition is a year in which the Olympic Warriors take place.
+    It has a host, a start date, an end date and a list of teams that participate in it.
+    """
+
+    year = models.IntegerField(validators=[MinValueValidator(2020), MaxValueValidator(2030)])
+    host = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    teams = models.ManyToManyField(Team, through="Registration")
+    is_active = models.BooleanField(default=True)
+
+
+class Registration(models.Model):
+    """
+    A registration is a link between a team and an edition. It is used to keep track of
+    the teams that participate in an edition.
+    """
+
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    edition = models.ForeignKey(Edition, on_delete=models.CASCADE)
+    confirmed = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
