fe5ea0bf1e563a7aa414c832d3bbc582b21952bd
Hugo Maurinier <hugo.maurinier@epitech.eu>
2024-04-05 19:38:59 +0200

    [ADD] Basic views and urls

--- olympic_warriors/urls.py
+++ olympic_warriors/urls.py
@@ -14,9 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("", views.home, name="home"),
+    path("profile/", views.profile, name="profile"),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- olympic_warriors/views.py
+++ olympic_warriors/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.http import require_GET, require_POST
 
-# Create your views here.
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+@require_GET
+@login_required
+def home(request):
+    return render(request, "olympic_warriors/home.html")
+
+
+@require_GET
+@login_required
+def profile(request):
+    return render(request, "olympic_warriors/profile.html")
