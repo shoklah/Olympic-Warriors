222d6e9c4d8e1acc533eb3bfba1b7525e2af68e2
Hugo Maurinier <hugo.maurinier@epitech.eu>
2024-07-02 17:27:34 +0200

    [ADD] Dockerized prod environment

--- server/olympic_warriors/settings.py
+++ server/olympic_warriors/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = settings.DEBUG
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
