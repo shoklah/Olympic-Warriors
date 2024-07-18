0abeadfcb43c6708db92567922de806d65457987
Hugo Maurinier <hugo.maurinier@epitech.eu>
2024-07-18 23:24:31 +0200

    [FIX] stage media files

--- server/olympic_warriors/settings.py
+++ server/olympic_warriors/settings.py
@@ -145,7 +145,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
 
 STORAGE = {
     "default": {
