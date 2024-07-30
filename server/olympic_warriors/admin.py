a46163664142f25f46075cdb8feb08274d2efeb4
Hugo Maurinier <hugo.maurinier@epitech.eu>
2024-07-30 12:27:53 +0200

    [ADD] Disciplines into admin dashboard

--- server/olympic_warriors/admin.py
+++ server/olympic_warriors/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import Player, Team, Edition
+from .models import Player, Team, Edition, Discipline, Rugby, RugbyEvent, Game, GameEvent, Crossfit
 
 admin.site.register(Player)
 admin.site.register(Team)
 admin.site.register(Edition)
+admin.site.register(Discipline)
+admin.site.register(Crossfit)
+admin.site.register(Rugby)
+admin.site.register(RugbyEvent)
+admin.site.register(Game)
+admin.site.register(GameEvent)
