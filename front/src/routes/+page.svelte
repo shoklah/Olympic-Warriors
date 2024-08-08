d7570d12de9776b7d25944ef7a5c859292bc72a6
Rodolphe <rodolpheandrieux@gmail.com>
2024-08-08 15:57:48 +0200

    OW-25 [build] landing page
    implemented the landing page design

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,7 +1,136 @@
 <script>
+    import { onMount } from 'svelte';
+
     import eclipse from "$lib/img/eclipse.png";
+    import title from "$lib/img/title.svg";
+
+    import blindtest from "$lib/img/icons/blindtest.svg";
+    import cachecache from "$lib/img/icons/cachecache.svg";
+    import crossfit from "$lib/img/icons/crossfit.svg";
+    import co from "$lib/img/icons/co.svg";
+    import dodgeball from "$lib/img/icons/dodgeball.svg";
+    import rugby from "$lib/img/icons/rugby.svg";
+
+    let countdownDate = new Date('September 21, 2024 09:00:00').getTime();
+    let days, hours, minutes, seconds;
+
+    const updateCountdown = () => {
+        const now = new Date().getTime();
+        const distance = countdownDate - now;
+
+        days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+        if (distance < 0) {
+            clearInterval(interval);
+            days = hours = minutes = seconds = 0;
+        }
+    };
+
+    let interval;
+    onMount(() => {
+        interval = setInterval(updateCountdown, 1000);
+        updateCountdown();
+        return () => clearInterval(interval);
+    });
 </script>
 
-<h1>Welcome to OW</h1>
+<div class="fullscreen">
+    <img id="eclipse" src={eclipse} alt="eclipse" />
+    <img id="title" src={title} alt="OLYMPIC WARRIORS">
 
-<img src={eclipse} alt="eclipse" />
+    <div class="sportcolumn">
+        <img src={rugby} alt="rugby">
+
+        <img src={cachecache} alt="cache cache">
+        <img src={crossfit} alt="crossfit">
+    </div>
+    <div class="sportcolumn">
+        <img src={co} alt="course d'orientation">
+        <img src={dodgeball} alt="dodgeball">
+        <img src={blindtest} alt="blindtest">
+    </div>
+</div>
+
+<div id="countdown">
+    <div><span>{days}</span>Jours</div>
+    <div><span>{hours}</span>Heures</div>
+    <div><span>{minutes}</span>Minutes</div>
+    <div><span>{seconds}</span>Secondes</div>
+</div>
+
+<style>
+    .sportcolumn {
+        position: absolute;
+        display: flex;
+        flex-direction: column;
+        opacity: .3;
+        gap: 180px;
+    }
+
+    .sportcolumn:first-of-type{
+        left: 20vw;
+    }
+
+    .sportcolumn:first-of-type :nth-child(2) {
+        transform: translate(-80px, 0);
+    }
+
+    .sportcolumn:last-of-type {
+        right: 20vw;
+    }
+
+    .sportcolumn:last-of-type :nth-child(2) {
+        transform: translate(80px, 0);
+    }
+
+    .sportcolumn img {
+        width: min(100px, 10vw);
+    }
+
+    #countdown {
+        display: flex;
+        justify-content: center;
+        gap: 2rem;
+        font-size: 1rem;
+        margin: 1rem 0;
+        color: var(--color-theme-1);
+    }
+
+    #countdown div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        width: 75px;
+    }
+
+    #countdown span {
+        font-size: 2.5rem;
+        font-weight: 600;
+        margin-bottom: 1rem;
+    }
+
+    #eclipse {
+        width: min(100%, 1200px);
+        margin: 0 auto;
+        transform: translate(1%, 0); /* to correct img default */
+        z-index: -10;
+    }
+
+    #title {
+        width: min(25%, 300px);
+        position: absolute;
+    }
+
+    .fullscreen {
+        position: relative;
+        height: 65vh;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+
+</style>
---
+++ front/src/routes/Header.svelte
@@ -0,0 +1,100 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/img/logo.svg';
+</script>
+
+<header>
+	<div id="logo">
+		<a href="/">
+			<img src={logo} alt="OW" />
+		</a>
+	</div>
+
+	<nav >
+<!--		<ul>-->
+<!--			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>-->
+<!--				<a href="/">Home</a>-->
+<!--			</li>-->
+<!--			<li aria-current={$page.url.pathname === '/login' ? 'page' : undefined}>-->
+<!--				<a href="/login">Login</a>-->
+<!--			</li>-->
+<!--		</ul>-->
+	</nav>
+</header>
+
+<style>
+	header {
+		display: flex;
+		padding: 2em clamp( 0em, 2vw, 5em);
+		justify-content: space-between;
+		view-transition-name: header;
+	}
+
+	#logo a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		height: 100%;
+	}
+
+	#logo img {
+		margin-left: 3vw;
+		height: 2.2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		view-transition-name: navbar;
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li[aria-current='page']::before {
+		--width: 30px;
+		--height: 3px;
+		content: '';
+		width: var(--width);
+		height: var(--height);
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--width) / 2);
+		background-color: var(--color-theme-1);
+		view-transition-name: indicator;
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 2em;
+		color: var(--color-theme-1);
+		font-weight: 700;
+		font-size: .8rem;
+		text-transform: uppercase;
+		letter-spacing: .2em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style>
