d995f3a77027b393b60fcb8e793be93c2badd80a
Hugo Maurinier <hugo.maurinier@epitech.eu>
2024-08-08 12:48:29 +0200

    [ADD] Automatically generated API Documentation

--- server/olympic_warriors/urls.py
+++ server/olympic_warriors/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 
 import os
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 from django.contrib import admin
 from django.urls import path, include
@@ -25,10 +26,18 @@ from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
+    # Django Admin
     path("admin/", admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", views.home, name="home"),
     path("profile/", views.profile, name="profile"),
+    path("players/", views.getPlayers, name="players"),
+    # API Documentation
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
+    ),
+    path("api/schema/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 
 env = os.environ.get("ENV", "dev").lower()
