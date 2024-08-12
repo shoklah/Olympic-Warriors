a3ec7caf4065519dbda0ab769891bca4ec526bc1
Hugo Maurinier <hugo.maurinier@epitech.eu>
2024-08-12 14:04:12 +0200

    [ADD] better refereeing distribution

--- server/olympic_warriors/models/Discipline.py
+++ server/olympic_warriors/models/Discipline.py
@@ -64,7 +64,77 @@ class Discipline(models.Model):
     def __str__(self) -> str:
         return self.name + " - " + str(self.edition.year)
 
-    def schedule_games(self):
+    def _assign_referees(
+        self, game_without_referees: list[Game], leftover_team_ids: set[int]
+    ) -> None:
+        """
+        Assign referees to games without referees while ensuring best possible distribution.
+
+        @param game_without_referees: list of games without referees for this round iteration
+        @param leftover_team_ids: set of team ids that can be referees for this round iteration
+        """
+        for game in game_without_referees:
+            best_referee_id = None
+            best_referee_score = None
+            for team_id in leftover_team_ids:
+                referee_score = Game.objects.filter(
+                    referees__id=team_id, discipline=self, is_active=True
+                ).count()
+                if not best_referee_id:
+                    best_referee_id = team_id
+                    best_referee_score = referee_score
+                elif referee_score < best_referee_score:
+                    best_referee_id = team_id
+                    best_referee_score = referee_score
+            game.referees = Team.objects.get(id=best_referee_id)
+            game.save()
+            leftover_team_ids.remove(best_referee_id)
+
+    def _create_round_iterations(
+        self,
+        l1: list[Team],
+        l2: list[Team],
+        leftover_team_ids: set[int],
+        game_round: TeamSportRound,
+        game_index: int,
+        iteration_index: int,
+        simultaneous_games: int,
+    ) -> int:
+        """
+        Create round iterations for team sports.
+
+        @param l1: list of teams in the first half of the round
+        @param l2: list of teams in the second half of the round
+        @param leftover_team_ids: set of team ids that can be referees for this round iteration
+        @param game_round: round of the game
+        @param game_index: index of the game in the round
+        @param iteration_index: index of the iteration in the round
+        @param simultaneous_games: number of simultaneous games in the round
+
+        @return: game index
+        """
+        games_without_referees = []
+
+        while game_index < simultaneous_games * (iteration_index + 1):
+            games_without_referees.append(
+                Game.objects.create(
+                    discipline=self,
+                    round=game_round,
+                    team1=l1[game_index],
+                    team2=l2[game_index],
+                    referees=l1[game_index],
+                    edition=self.edition,
+                )
+            )
+            leftover_team_ids.remove(l1[game_index].id)
+            leftover_team_ids.remove(l2[game_index].id)
+            game_index += 1
+
+        self._assign_referees(games_without_referees, leftover_team_ids)
+
+        return game_index
+
+    def schedule_games(self) -> None:
         """
         Schedule round-robin games for the discipline, including teams refereeing.
         Applicable to team sports.
@@ -83,27 +153,16 @@ class Discipline(models.Model):
             game_round = TeamSportRound.objects.create(discipline=self, order=round_index)
             game_index = 0
             for iteration_index in range(iteration_per_round):
-                games_without_referees = []
-                leftover_team_ids = deepcopy(team_ids)
 
-                while game_index < simultaneous_games * (iteration_index + 1):
-                    games_without_referees.append(
-                        Game.objects.create(
-                            discipline=self,
-                            round=game_round,
-                            team1=l1[game_index],
-                            team2=l2[game_index],
-                            referees=l1[game_index],
-                            edition=self.edition,
-                        )
-                    )
-                    leftover_team_ids.remove(l1[game_index].id)
-                    leftover_team_ids.remove(l2[game_index].id)
-                    game_index += 1
-
-                for game in games_without_referees:
-                    game.referees = Team.objects.get(id=leftover_team_ids.pop())
-                    game.save()
+                game_index = self._create_round_iterations(
+                    l1,
+                    l2,
+                    deepcopy(team_ids),
+                    game_round,
+                    game_index,
+                    iteration_index,
+                    simultaneous_games,
+                )
 
             l2.append(l1.pop())
             l1.insert(1, l2.pop(0))
