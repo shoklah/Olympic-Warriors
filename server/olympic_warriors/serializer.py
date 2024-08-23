e300282efeed469de9fdd14526ba5bdbabfb19df
Hugo Maurinier <hugo.maurinier@epitech.eu>
2024-08-23 16:18:28 +0200

    [ADD] Game Serializer

--- server/olympic_warriors/serializer.py
+++ server/olympic_warriors/serializer.py
@@ -3,13 +3,14 @@ Serializers for the Olympic Warriors app
 """
 
 from rest_framework import serializers
-from olympic_warriors.models import Player, Edition, Team, Discipline, PlayerRating
+from olympic_warriors.models import Player, Edition, Team, Discipline, PlayerRating, Game
 
 
 class PlayerSerializer(serializers.ModelSerializer):
     """
     Player serializer
     """
+
     first_name = serializers.CharField(source="user.first_name")
     last_name = serializers.CharField(source="user.last_name")
 
@@ -70,4 +71,10 @@ class PlayerRatingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PlayerRating
+        fields = "__all__"
+
+
+class GameSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Game
         fields = "__all__"
