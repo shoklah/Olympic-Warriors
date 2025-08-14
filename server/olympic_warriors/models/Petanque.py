21fc19b27b477544d85c5d1f7ccfa5233345f0e3
Hugo Maurinier <hugo.maurinier@epitech.eu>
2025-08-14 19:06:33 +0200

    [ADD] Petanque model

---
+++ server/olympic_warriors/models/Petanque.py
@@ -0,0 +1,22 @@
+from django.db import models
+from django.core.exceptions import ValidationError
+
+from .Discipline import Discipline, Game, GameEvent
+from .ResultTypes import ResultTypes
+
+
+class Petanque(Discipline):
+    """
+    Petanque is a discipline that takes place in an edition of the Olympic Warriors.
+    """
+
+    def save(self, *args, **kwargs):
+        """
+        Override save method to set discipline name to petanque, schedule games and initialize team results.
+        """
+        # Check if the object is already in the database
+        if self.pk is None:
+            self.name = 'Petanque'
+            self.result_type = ResultTypes.POINTS
+
+        super().save(*args, **kwargs)
